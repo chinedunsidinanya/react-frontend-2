4a6ff543ead94d857a76d7e75f72ebd41adde649
Chinedu Nsidinanya <chineducarson@icloud.com>
2021-12-16 21:04:08 -0600

    Doing a new build for production deployment

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React and become a Production expert
         </a>
       </header>
     </div>
